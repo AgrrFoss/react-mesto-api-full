d15024cda0b5b0e506a69b1354b65fc0837e8a33
Gregori Orlov <Orlov.Gregori@yandex.ru>
2022-09-23 17:13:27 +0300

    вывел токены в ошибку

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -94,7 +94,7 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        sameSite: 'none',
+        sameSite: 'None',
       })
         .send({ token })
         .end();
