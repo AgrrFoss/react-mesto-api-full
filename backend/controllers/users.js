0e399133a91629c29107bd6092f51f91bfc4c470
Gregori Orlov <Orlov.Gregori@yandex.ru>
2022-09-23 22:40:49 +0300

    исправить букв у в none

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -94,7 +94,7 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        sameSite: 'None',
+        sameSite: 'none',
         secure: true,
       })
         .send({ token })
