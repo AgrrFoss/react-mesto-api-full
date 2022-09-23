990f1ceafadb3d862a1bf55f2ebf37761ed63589
Gregori Orlov <Orlov.Gregori@yandex.ru>
2022-09-23 18:18:04 +0300

    добавить secure: true,

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -95,6 +95,7 @@ module.exports.login = (req, res, next) => {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
         sameSite: 'None',
+        secure: true,
       })
         .send({ token })
         .end();
